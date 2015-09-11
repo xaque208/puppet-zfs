bc469429bdea46fd002debdd09ece11010312314
Zach Leslie <zach@puppetlabs.com>
2015-09-11 04:20:07 +0000

    Lint the snapshot

--- manifests/snapshots.pp
+++ manifests/snapshots.pp
@@ -2,9 +2,9 @@ class zfs::snapshots {
 
   file { '/usr/local/bin/zfs-snapshot.rb':
     source => 'puppet:///modules/zfs/zfs-snapshot.rb',
-    owner  => root,
-    group  => 0,
-    mode   => 0750,
+    owner  => 'root',
+    group  => '0',
+    mode   => '0750',
   }
 
   $env = $operatingsystem ? {
@@ -16,24 +16,24 @@ class zfs::snapshots {
 
   cron {
     'zfs hourly snapshot':
-      user        => root,
-      minute      => 5,
-      command     => "/usr/local/bin/zfs-snapshot.rb -r -c 25 -s hourly",
+      user        => 'root',
+      minute      => '5',
+      command     => '/usr/local/bin/zfs-snapshot.rb -r -c 25 -s hourly',
       environment => $env,
       require     => File['/usr/local/bin/zfs-snapshot.rb'];
     'zfs daily snapshot':
-      user        => root,
-      minute      => 10,
-      hour        => 1,
-      command     => "/usr/local/bin/zfs-snapshot.rb -r -c 8 -s daily",
+      user        => 'root',
+      minute      => '10',
+      hour        => '1',
+      command     => '/usr/local/bin/zfs-snapshot.rb -r -c 8 -s daily',
       environment => $env,
       require     => File['/usr/local/bin/zfs-snapshot.rb'];
     'zfs weekly snapshot':
-      user        => root,
-      minute      => 15,
-      hour        => 2,
-      weekday     => 0,
-      command     => "/usr/local/bin/zfs-snapshot.rb -r -c 5 -s weekly",
+      user        => 'root',
+      minute      => '15',
+      hour        => '2',
+      weekday     => '0',
+      command     => '/usr/local/bin/zfs-snapshot.rb -r -c 5 -s weekly',
       environment => $env,
       require     => File['/usr/local/bin/zfs-snapshot.rb'];
   }
